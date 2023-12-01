5b47af9664834c04daed17aaa7c6170ec8cf1dc1
timofey <tim@ongoteam.yaconnect.com>
2023-12-01 19:00:10 +0000

    pull fuzz

--- tests/pull.rs
+++ tests/pull.rs
@@ -3,6 +3,7 @@ use std::{pin::pin, task::Context};
 use async_channel::bounded;
 use common::{channel_pair, Channel};
 use futures_util::{Future, SinkExt};
+use fuzzer::{Local, Runner};
 use stream_patterns::pull;
 
 mod common;
@@ -27,4 +28,30 @@ fn pull_one() {
     drop(pusher);
     assert!(pulling.as_mut().poll(&mut cx).is_ready());
     assert!(done_r.try_recv().unwrap().is_none());
+}
+
+fn pull_one_seed(seed: u64) {
+    let mut runner = Runner::new(seed);
+    let (puller, pusher) = runner.channel();
+    let (ready_s, ready_r) = bounded(10);
+    let (done_s, done_r) = bounded(10);
+    let (msg_s, msg_r) = bounded(10);
+    let mut pulling = pin!(pull::<Local<i32>>(ready_r, done_s, msg_s));
+    ready_s.try_send(puller).unwrap();
+    pusher.send(426);
+    runner.pending_in(100, pulling.as_mut());
+    let (msg, promise) = msg_r.try_recv().unwrap();
+    assert_eq!(msg, 426);
+    promise.done();
+    ready_s.close();
+    drop(pusher);
+    runner.ready_in(100, pulling.as_mut());
+    assert!(done_r.try_recv().unwrap().is_none());
+}
+
+#[test]
+fn pull_one_fuzz() {
+    for i in 0..1000 {
+        pull_one_seed(i);
+    }
 }
